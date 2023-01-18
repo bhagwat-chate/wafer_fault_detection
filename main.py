bdfad899e18e0d9f114fb1e6dd30c0e92be52f66
bhagwat-chate <bhagwat.chate25@gmail.com>
2023-01-18 17:06:08 +0530

    new approach for prediction

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
+from wafer.pipeline.data_ingestion_pipeline import TrainingDataPipeline
+from wafer.pipeline.model_training_pipeline import Model_Training_Pipeline
 from wafer.logger import logging
 from wafer.exception import WaferException
 import sys
 
-from wafer.pipeline.data_ingestion_pipeline import TrainingDataPipeline
-from wafer.pipeline.model_training_pipeline import Model_Training_Pipeline
-from wafer.pipeline.prediction_pipeline import Prediction_Pipeline
 
 if __name__ == '__main__':
     try:
         logging.info("START OF WAFER FAULT DETECTION PROJECT EXECUTION")
 
-        # data_ingestion = TrainingDataPipeline()
-        # data_ingestion.train_data_validation()
+        data_ingestion = TrainingDataPipeline()
+        data_ingestion.train_data_validation()
 
         model_training = Model_Training_Pipeline()
         model_training.train_model()
-
-        # prediction = Prediction_Pipeline()
-        # prediction.prediction_pipeline_execute()
 
         logging.info("END OF WAFER FAULT DETECTION PROJECT EXECUTION")
 
     except WaferException as e:
         raise WaferException(e, sys)
-
-
