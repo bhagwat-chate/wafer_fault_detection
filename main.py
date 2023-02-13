e343167bbaacc8282a3818402770a0109840b6f0
bhagwat-chate <bhagwat.chate25@gmail.com>
2023-02-13 19:27:23 +0530

    update

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
         prediction_pipeline.prediction_pipeline_execution('path')
 
         # app.run(host="0.0.0.0", port=5000)
-        # logging.info("END OF WAFER FAULT DETECTION PROJECT EXECUTION")
+        logging.info("END OF WAFER FAULT DETECTION PROJECT EXECUTION")
 
     except WaferException as e:
         raise WaferException(e, sys)
